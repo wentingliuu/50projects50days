cb5fe44a407df9e16ac1484921dda961fa74e6ff
wentingliu <cindy810317@gmail.com>
2022-03-29 01:31:50 +0800

    06-scroll-animation

---
+++ 06-scroll-animation/script.js
@@ -0,0 +1,17 @@
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+  const triggerBottom = window.innerHeight / 5 * 4
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top
+    if(triggerBottom > boxTop) {
+      box.classList.add('show')
+    } else {
+      box.classList.remove('show')
+    }
+  })
+}
