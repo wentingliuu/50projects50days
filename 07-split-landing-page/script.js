42f67eab655a85e0ee3356204b134d86344229d5
wentingliu <cindy810317@gmail.com>
2022-03-31 00:30:36 +0800

    07-split-landing-page-fixed

--- 07-split-landing-page/script.js
+++ 07-split-landing-page/script.js
@@ -4,6 +4,10 @@ const container = document.querySelector('.container')
 
 left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
 left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
+left.addEventListener('touchstart', () => container.classList.add('hover-left'))
+left.addEventListener('touchend', () => container.classList.remove('hover-left'))
 
 right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
 right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
+right.addEventListener('touchstart', () => container.classList.add('hover-right'))
+right.addEventListener('touchend', () => container.classList.remove('hover-right'))
