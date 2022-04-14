bbaf01073c98ea576ba72f576c6dc678960e5a76
wentingliu <cindy810317@gmail.com>
2022-04-14 13:21:40 +0800

    15-incrementing-counter

---
+++ 15-incrementing-counter/script.js
@@ -0,0 +1,22 @@
+const counters = document.querySelectorAll('.counter')
+
+counters.forEach(counter => {
+  counter.innerText = '0'
+
+  const updateCounter = () => {
+    // use + to let the strings become numbers
+    // JavaScript Type Conversion
+    const target = +counter.getAttribute('data-target')
+    const c = +counter.innerText
+    const increment = target / 300
+
+    if (c < target) {
+      counter.innerText = `${Math.ceil(c + increment)}`
+      setTimeout(updateCounter, 1)
+    } else {
+      counter.innerText = target
+    }
+  }
+
+  updateCounter()
+})
